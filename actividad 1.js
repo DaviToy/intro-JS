f694ece4c26a351d0d7b5fd9ffc498e51379f215
David Serrano <pitgear100@gmail.com>
2022-04-26 21:23:34 -0500

    Actividad 1 modificado con alert

--- actividad 1.js
+++ actividad 1.js
@@ -2,22 +2,26 @@
 num1 = prompt("escribe un numero")
 '2'
 var multi = num1 * 2
+alert("Resultado: "+multi)
 console.log(multi)
 
 //Actividad 1.2
 var num1 = prompt("Escribe el primer numero")
 var num2 = prompt("Escribe el segundo numero")
 var resta = num1 - num2
+alert("Resultado : "+resta)
 console.log(resta)
 
 // Actividad 1.3
 var pi = 3.1416
 var r = 5.3
 var area = (pi*(r*r))
+alert("Resultado: "+area)
 console.log(area)
 
 //Actividad 1.4
 var D = 8
 var d = 6
 var arear = (D*d)/2
+alert("Resultado: "+arear)
 console.log(arear)
