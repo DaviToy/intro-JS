00dad4fc37d471ff92c17cb16305749563ea4f1d
David Serrano <pitgear100@gmail.com>
2022-04-26 21:08:19 -0500

    actividad 1

---
+++ actividad 1.js
@@ -0,0 +1,23 @@
+//Actividad 1.1
+num1 = prompt("escribe un numero")
+'2'
+var multi = num1 * 2
+console.log(multi)
+
+//Actividad 1.2
+var num1 = prompt("Escribe el primer numero")
+var num2 = prompt("Escribe el segundo numero")
+var resta = num1 - num2
+console.log(resta)
+
+// Actividad 1.3
+var pi = 3.1416
+var r = 5.3
+var area = (pi*(r*r))
+console.log(area)
+
+//Actividad 1.4
+var D = 8
+var d = 6
+var arear = (D*d)/2
+console.log(arear)
